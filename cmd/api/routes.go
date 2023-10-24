0b3496a17d069bc22b8626448b0cad7b22752233
Syful Sharif <sharif.syful@gmail.com>
2023-10-24 14:34:25 +0600

    MiddleWare Updated

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 	// create a router mux
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
+	mux.Use(app.enableCORS)
 	mux.Get("/", app.Home)
 	mux.Get("/movies", app.AllMovies)
 	return mux
